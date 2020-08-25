324bd134cee50e64fcf67da697c3069070ee5cb4
askario95 <gazizow37@gmail.com>
2020-08-25 17:51:54 +0500

    chapter_007 - Wget

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -9,7 +9,7 @@ public class Wget {
                             System.out.print("\rLoading : " + index + "%");
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
                 }
