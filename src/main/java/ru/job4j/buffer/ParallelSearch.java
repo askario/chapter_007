a8b827e741c7988bf82585bd823d5b784a0d2539
askario95 <gazizow37@gmail.com>
2020-10-07 02:40:28 +0500

    Chapter007: Interrupt consumer

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.job4j.buffer;
+
+import ru.job4j.concurrent.prodcons.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+
+        final Thread consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    System.out.println(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        consumer.start();
+
+        final Thread producer = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                queue.offer(index);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            consumer.interrupt();
+        });
+
+        producer.start();
+    }
+}
