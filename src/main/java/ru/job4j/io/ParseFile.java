6b5cb945cb6e7eaafb38b1e6373502005166bcc7
askario95 <gazizow37@gmail.com>
2020-10-13 23:59:34 +0500

    Chapter007: ParseFile - changes according to review

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,17 +1,18 @@
 package ru.job4j.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class ParseFile {
     private File file;
-
-    public synchronized void setFile(File f) {
-        file = f;
-    }
-
-    public synchronized File getFile() {
-        return file;
-    }
 
     public synchronized String getContent() throws IOException {
         try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file), 1024)) {
@@ -37,18 +38,26 @@ public class ParseFile {
         }
     }
 
-    public synchronized void saveContent(String content) throws IOException {
-        OutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         ParseFile parseFile = new ParseFile();
         parseFile.setFile(new File("pom_tmp.xml"));
 
         System.out.println(parseFile.getContent());
         System.out.println(parseFile.getContentWithoutUnicode());
+    }
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class ContentSaver {
+    private File file;
+
+    public synchronized void saveContent(String content) throws IOException {
+        OutputStream o = new FileOutputStream(file);
+        for (int i = 0; i < content.length(); i += 1) {
+            o.write(content.charAt(i));
+        }
     }
 }
